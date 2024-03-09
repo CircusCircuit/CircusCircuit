9b1e86eedae7289126f60532e96aa720c223239b
been <ideal402@naver.com>
2024-03-09 16:35:57 +0900

    총알 생성 패턴 추가

--- Assets/02.Scripts/EnemyMove.cs
+++ Assets/02.Scripts/EnemyMove.cs
@@ -6,6 +6,7 @@ public class EnemyMove : MonoBehaviour
 {
     Rigidbody2D rigid; 
     SpriteRenderer spriteRenderer;
+    public GameObject bulletPrefab;
     public int nextmove;
 
     // Start is called before the first frame update
@@ -33,6 +34,9 @@ public class EnemyMove : MonoBehaviour
         if(rayHit.collider == null){
             Turn();
         }
+
+        //총알발사
+        FireBullet();
     }
 
     //몬스터 행동 결정 함수, 재귀
@@ -56,5 +60,15 @@ public class EnemyMove : MonoBehaviour
         spriteRenderer.flipX = nextmove == 1;
         CancelInvoke("Think");
         Invoke("Think",2);
+    }
+
+    void FireBullet()
+    {
+        Debug.Log("fire!");
+        if (Random.value < 0.01f) // 1% 확률로 발사
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity); // 총알 생성
+            bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(nextmove * 10, 0); // 총알 이동 설정
+        }
     }
 }

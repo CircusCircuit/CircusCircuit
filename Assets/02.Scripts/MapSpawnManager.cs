ecb034a9ce01ecfa43ad76bbbf874bb697b85534
muncaem <mooncm02@gmail.com>
2024-05-26 17:01:37 +0900

    맵 랜덤 스폰 구현

--- Assets/02.Scripts/MapSpawnManager.cs
+++ Assets/02.Scripts/MapSpawnManager.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class MapSpawnManager : MonoBehaviour
 {
-    [SerializeField] List<GameObject> maps;
-    [SerializeField] Transform PlayerPos;
+    //[SerializeField] List<GameObject> maps;
+    //[SerializeField] Transform PlayerPos;
+    [SerializeField] Camera cam;
+    [System.Serializable]
+    public class MapArray
+    {
+        [SerializeField] int row;
+        public int Row
+        {
+            get { return row; }
+            set { row = value; }
+        }
+
+        [SerializeField] int col;
+        public int Col
+        {
+            get { return col; }
+            set { col = value; }
+        }
+    }
+    [SerializeField] List<MapArray> stageArray;
+
+    List<MapArray> dynamicStageArray;
 
     private void Start()
     {
-        Spawn();
+        Allocation();
+        MapSpawn();
     }
 
     // Update is called once per frame
@@ -17,20 +39,42 @@ public class MapSpawnManager : MonoBehaviour
     {
         if (GameManager.Instance.getNextWave)
         {
-            Destroy(GameObject.FindWithTag("Stage"));
-            Spawn();
+            MapSpawn();
 
             GameManager.Instance.getNextWave = false;
-            
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Escape))
+        //{
+        //    MapSpawn();
+
+        //    if (dynamicStageArray.Count <= 0)
+        //    {
+        //        Allocation();
+        //    }
+        //}
+    }
+
+    void Allocation()
+    {
+        dynamicStageArray = new List<MapArray>(new MapArray[stageArray.Count]);
+        for (int i = 0; i < stageArray.Count; i++)
+        {
+            dynamicStageArray[i] = stageArray[i];
         }
     }
 
-    void Spawn()
+    void MapSpawn()
     {
-        int rIndex = Random.Range(0, maps.Count);
-        Instantiate(maps[rIndex]);
-        maps.RemoveAt(rIndex);
+        //int rIndex = Random.Range(0, maps.Count);
+        //Instantiate(maps[rIndex]);
+        //maps.RemoveAt(rIndex);
 
-        PlayerPos.localPosition = GameObject.FindWithTag("SpawnPoint").transform.position;
+        //PlayerPos.localPosition = GameObject.FindWithTag("SpawnPoint").transform.position;
+
+        int rIndex = Random.Range(0, dynamicStageArray.Count);
+        cam.transform.position = new Vector2(dynamicStageArray[rIndex].Row, dynamicStageArray[rIndex].Col);
+
+        dynamicStageArray.RemoveAt(rIndex);
     }
 }
